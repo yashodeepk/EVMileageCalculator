b8346ad774ba953d783232a426cc8e90d43cc192
Yashodeep Kacholiya <yashodeepkacholiya@Yashodeeps-MacBook-Air.local>
2021-07-20 13:16:12 +0530

    converts data model to flspot and proper hower value

--- lib/pages/analaticsPage.dart
+++ lib/pages/analaticsPage.dart
@@ -12,11 +12,6 @@ class analatics extends StatefulWidget {
   _analaticsState createState() => _analaticsState();
 }
 
-List<Color> gradientColors = [
-  const Color(0xff23b6e6),
-  const Color(0xff02d39a),
-];
-
 class _analaticsState extends State<analatics> {
   Future<List<DataModel>>? data;
   List<DataModel> datas = [];
@@ -24,6 +19,10 @@ class _analaticsState extends State<analatics> {
   bool? fetching = true;
   var logo;
 
+  List<Color> gradientColors = [
+    const Color(0xFF03ADC6),
+    const Color(0xff02d39a),
+  ];
   @override
   void initState() {
     super.initState();
@@ -39,9 +38,28 @@ class _analaticsState extends State<analatics> {
     }
   }
 
+  List<FlSpot> convetDataModelToFlSpot() {
+    List<FlSpot> arrSpots = [];
+    var length;
+
+    if (datas.length > 15)
+      length = 15;
+    else
+      length = datas.length;
+
+    print("Data Length is " + datas.length.toString());
+    for (int dataIndex = 0; dataIndex < length; dataIndex++) {
+      arrSpots.add(FlSpot(double.parse(dataIndex.toString()),
+          double.parse(datas[dataIndex].distance.toString()) / 10));
+    }
+    print("Arr Spots is " + arrSpots.toString());
+    return arrSpots;
+  }
+
   void getdata() async {
     data = db!.getData();
     datas = await db!.getData();
+    datas = datas.reversed.toList();
     setState(() {
       fetching = false;
     });
@@ -200,109 +218,134 @@ class _analaticsState extends State<analatics> {
       ),
     );
   }
-}
 
-LineChartData mainData() {
-  return LineChartData(
-    gridData: FlGridData(
-      show: true,
-      drawVerticalLine: true,
-      getDrawingHorizontalLine: (value) {
-        return FlLine(
-          color: const Color(0xff37434d),
-          strokeWidth: 1,
-        );
-      },
-      getDrawingVerticalLine: (value) {
-        return FlLine(
-          color: const Color(0xff37434d),
-          strokeWidth: 1,
-        );
-      },
-    ),
-    titlesData: FlTitlesData(
-      show: true,
-      bottomTitles: SideTitles(
-        showTitles: true,
-        reservedSize: 22,
-        getTextStyles: (value) => const TextStyle(
-            color: Color(0xff68737d),
-            fontWeight: FontWeight.bold,
-            fontSize: 16),
-        getTitles: (value) {
-          switch (value.toInt()) {
-            case 2:
-              return 'T2';
-            case 5:
-              return 'T5';
-            case 8:
-              return 'T8';
-            case 11:
-              return 'T11';
-            case 14:
-              return 'T14';
-          }
-          return '';
-        },
-        margin: 8,
-      ),
-      leftTitles: SideTitles(
-        showTitles: true,
-        getTextStyles: (value) => const TextStyle(
-          color: Color(0xff67727d),
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
+  LineChartData mainData() {
+    return LineChartData(
+      lineTouchData: LineTouchData(
+        touchTooltipData: LineTouchTooltipData(
+          tooltipBgColor: Colors.blueGrey.withOpacity(0.8),
+          getTooltipItems: (List<LineBarSpot> touchedBarSpots) {
+            return touchedBarSpots.map((barSpot) {
+              final flSpot = barSpot;
+              return LineTooltipItem(
+                '${datas[flSpot.x.toInt()].title} \n',
+                const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                children: [
+                  TextSpan(
+                    text: (flSpot.y * 10).toString(),
+                    style: TextStyle(
+                      color: Colors.grey[100],
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' km ',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              );
+            }).toList();
+          },
         ),
-        getTitles: (value) {
-          switch (value.toInt()) {
-            case 1:
-              return '10';
-            case 3:
-              return '30';
-            case 5:
-              return '50';
-            case 7:
-              return '70';
-            case 9:
-              return '90';
-          }
-          return '';
-        },
-        reservedSize: 28,
-        margin: 12,
       ),
-    ),
-    borderData: FlBorderData(
+      gridData: FlGridData(
         show: true,
-        border: Border.all(color: const Color(0xff37434d), width: 1)),
-    minX: 0,
-    maxX: 15,
-    minY: 0,
-    maxY: 10,
-    lineBarsData: [
-      LineChartBarData(
-        spots: [
-          FlSpot(0, 3),
-          FlSpot(2, 2),
-          FlSpot(4, 5),
-          FlSpot(6, 3.1),
-          FlSpot(8, 4),
-          FlSpot(9, 3),
-          FlSpot(11, 4),
-        ],
-        isCurved: false,
-        colors: gradientColors,
-        barWidth: 5,
-        isStrokeCapRound: true,
-        dotData: FlDotData(
-          show: true,
+        drawVerticalLine: true,
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          getTextStyles: (value) => const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 16),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return 'T2';
+              case 4:
+                return 'T5';
+              case 7:
+                return 'T8';
+              case 10:
+                return 'T11';
+              case 13:
+                return 'T14';
+            }
+            return '';
+          },
+          margin: 8,
         ),
-        belowBarData: BarAreaData(
-          show: true,
-          colors:
-              gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return '10';
+              case 3:
+                return '30';
+              case 5:
+                return '50';
+              case 7:
+                return '70';
+              case 9:
+                return '90';
+            }
+            return '';
+          },
+          reservedSize: 28,
+          margin: 12,
         ),
       ),
-    ],
-  );
+      borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: 14,
+      minY: 0,
+      maxY: 10,
+      lineBarsData: [
+        LineChartBarData(
+          spots: convetDataModelToFlSpot(),
+          isCurved: false,
+          colors: gradientColors,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: true,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            colors:
+                gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
+        ),
+      ],
+    );
+  }
 }
